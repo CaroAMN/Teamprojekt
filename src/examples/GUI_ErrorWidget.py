d5da5d100e805091cc3e0596bc332f475eef133d
Hristian-Gabrovski <hristian.gabrovski@gmx.de>
2020-05-23 10:41:45 +0200

    black test

--- src/examples/GUI_ErrorWidget.py
+++ src/examples/GUI_ErrorWidget.py
@@ -5,9 +5,10 @@ import numpy as np
 
 
 from GUI_EXAMPLE_BASE import GUI_EXAMPLE_BASE
-sys.path.insert(0, '../view')
 
-if __name__ == '__main__':
+sys.path.insert(0, "../view")
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     ex = GUI_EXAMPLE_BASE()  # plain QMainWindow with basic layout and menu bar
 
@@ -17,7 +18,20 @@ if __name__ == '__main__':
     y = np.array([-2, 3, -8, -4, 3, 12, -15, 10, 5, -12])
     green = (0, 255, 0)
     purple = (128, 0, 255)
-    c = np.array([green, purple, green, purple, green, purple, green, purple, green, green])
+    c = np.array(
+        [
+            green,
+            purple,
+            green,
+            purple,
+            green,
+            purple,
+            green,
+            purple,
+            green,
+            green,
+        ]
+    )
     example_widget.setMassErrors(x, y, c)
     ex.setExampleWidget(example_widget)
     ex.show()
