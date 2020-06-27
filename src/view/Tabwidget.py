d084ec2761b2215e385cbbb4c98adf70c64d39e9
Carolin Schwitalla <carolin-schwitalla@web.de>
2020-06-27 18:20:38 +0200

    Tabwidget erstellt mit protein viewer in tab 1, andere noch leer

---
+++ src/view/Tabwidget.py
@@ -0,0 +1,26 @@
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+import sys
+from Gui_1 import Window
+
+class TabWindow(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+        layout = QGridLayout()
+        self.setLayout(layout)
+        label1 = QLabel("Widget in Tab 1.")
+        label2 = QLabel("Widget in Tab 2.")
+        protein_viewer = Window()
+        tabwidget = QTabWidget()
+        tabwidget.addTab(protein_viewer, "Protein viewer")
+        tabwidget.addTab(label2, "Tab 2")
+        layout.addWidget(tabwidget, 0, 0)
+
+def main():
+    app = QApplication(sys.argv)
+    screen = TabWindow()
+    screen.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
