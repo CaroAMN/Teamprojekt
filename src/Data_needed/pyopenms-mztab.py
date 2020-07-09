ff0a45c197a70fab0ec1919bfe7dc2c4c59a6428
Hristian-Gabrovski <hristian.gabrovski@gmx.de>
2020-07-09 12:03:33 +0200

    LFQ wird über Python file ausgeführt

--- src/Data_needed/pyopenms-mztab.py
+++ src/Data_needed/pyopenms-mztab.py
@@ -1,32 +1,6 @@
 import sys
+import os
 from pyopenms import *
 
-# loading spectrum data
-exp = MSExperiment()
-MzMLFile().load("BSA1_F1.mzML", exp)
-# accessing spectrum data
-spectrum_data = exp.getSpectrum(0).get_peaks()
-print(spectrum_data)
-
-# loadin protein data form idXML file
-protein_ids = []
-peptide_ids = []
-IdXMLFile().load("BSA1_F1.idXML", protein_ids, peptide_ids)
-# each protein/peptide object has Hit object
-hits_listpro = protein_ids[0].getHits()
-hits_listpep = peptide_ids[0].getHits()
-# getting Accession and Sequence from the hit objects
-print(hits_listpro[0].getAccession())
-print(hits_listpep[0].getSequence())
-
-# searching with databank and mzML File
-# Prints result automatically
-protein_ids2 = []
-peptide_ids2 = []
-SimpleSearchEngineAlgorithm().search(
-    "BSA1_F1.mzML", "18Protein_SoCe_Tr_detergents_trace_target_decoy.fasta", protein_ids2, peptide_ids2)
-
-# going trough the peptide list and printing MZ + RT from each pephit
-for pep_id in peptide_ids2:
-    print("the MZ: ", pep_id.getMZ())
-    print("the RT: ", pep_id.getRT())
+os.system("""ProteomicsLFQ -in BSA1.mzML BSA2.mzML BSA3.mzML -ids BSA1_F1.idXML BSA2_F1.idXML BSA3_F1.idXML  -design experimental_design.tsv -fasta 18Protein_SoCe_Tr_detergents_trace_target_decoy.fasta  -out_cxml BSA.consensusXML.tmp        -out_msstats BSA.csv.tmp        -out BSA.mzTab.tmp
+ """)
