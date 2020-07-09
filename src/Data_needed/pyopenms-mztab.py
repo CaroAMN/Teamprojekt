513653eff843a170a1cd3cf9248b377669def3a8
Hillengas <alexander_gebhard@web.de>
2020-07-09 14:01:47 +0200

    Minimal angepasste/ veränderte Ausführung für ProteomicsLFQ.

--- src/Data_needed/pyopenms-mztab.py
+++ src/Data_needed/pyopenms-mztab.py
@@ -2,5 +2,5 @@ import sys
 import os
 from pyopenms import *
 
-os.system("""ProteomicsLFQ -in BSA1.mzML BSA2.mzML BSA3.mzML -ids BSA1_F1.idXML BSA2_F1.idXML BSA3_F1.idXML  -design experimental_design.tsv -fasta 18Protein_SoCe_Tr_detergents_trace_target_decoy.fasta  -out_cxml BSA.consensusXML.tmp        -out_msstats BSA.csv.tmp        -out BSA.mzTab.tmp
- """)
+os.system("""ProteomicsLFQ -in BSA1.mzML BSA2.mzML BSA3.mzML -ids BSA1_OMSSA.idXML BSA2_OMSSA.idXML BSA3_OMSSA.idXML  -design experimental_design.tsv -fasta 18Protein_SoCe_Tr_detergents_trace_target_decoy.fasta  -out_cxml BSA.consensusXML.tmp        -out_msstats BSA.csv.tmp        -out BSA.mzTab.tmp   -threads 1   -proteinFDR 0.3
+""")
